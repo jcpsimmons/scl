f0c91b7be0aaf0c66e0c7f784ddc9da3c14ba7aa
josh simmons <jcpsimmons@gmail.com>
2025-12-27 13:25:28 -0700

    Auto: Edit index.ts

--- src/components/ui/dithered-image/index.ts
+++ src/components/ui/dithered-image/index.ts
@@ -1,2 +1,2 @@
 export { DitheredImage } from './dithered-image'
-export type { DitheredImageProps } from './dithered-image'
+export type { DitheredImageProps, DitherAlgorithm } from './dithered-image'
