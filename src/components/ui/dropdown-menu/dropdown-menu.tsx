c13bd705793b8ffbb4a1d1ffb155a3b74ec01bd8
josh simmons <jcpsimmons@gmail.com>
2025-12-27 15:01:33 -0700

    Auto: Edit dropdown-menu.tsx

--- src/components/ui/dropdown-menu/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu/dropdown-menu.tsx
@@ -74,7 +74,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-primary/20 focus:bg-primary/20 focus:text-primary data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0',
+      'relative flex cursor-default select-none items-center gap-2 px-2 py-1.5 text-sm outline-none transition-colors hover:bg-primary/20 focus:bg-primary/20 focus:text-primary data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0',
       inset && 'pl-8',
       className
     )}
