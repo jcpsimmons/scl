450d82fb494999090b3dc0a797842184571a3bd9
josh simmons <jcpsimmons@gmail.com>
2025-12-28 16:03:00 -0700

    Auto: Edit card.tsx

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'border bg-card text-card-foreground',
+      'border-2 bg-card text-card-foreground',
       className
     )}
     {...props}
