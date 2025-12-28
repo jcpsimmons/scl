20c1d54805fbab0256f4ea82e4e7fa02c3b6bb4c
josh simmons <jcpsimmons@gmail.com>
2025-12-28 16:03:32 -0700

    Auto: Edit scroll-area.tsx

--- src/components/ui/scroll-area/scroll-area.tsx
+++ src/components/ui/scroll-area/scroll-area.tsx
@@ -38,7 +38,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 bg-primary terminal-glow" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 bg-primary" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
