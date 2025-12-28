0a37dc9c3e4b184abd7b4a4b4451d82e8d643894
josh simmons <jcpsimmons@gmail.com>
2025-12-28 09:58:42 -0700

    Auto: Edit combobox.tsx

--- src/components/ui/combobox/combobox.tsx
+++ src/components/ui/combobox/combobox.tsx
@@ -62,7 +62,9 @@ const Combobox = React.forwardRef<HTMLButtonElement, ComboboxProps>(
             className={cn('w-[200px] justify-between', className)}
             disabled={disabled}
           >
-            {selectedOption ? selectedOption.label : placeholder}
+            <span className="truncate">
+              {selectedOption ? selectedOption.label : placeholder}
+            </span>
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
