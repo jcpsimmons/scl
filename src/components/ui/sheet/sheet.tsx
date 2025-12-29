10f0cf80455e5f402d222a5d45b8621dc9ca6147
josh simmons <jcpsimmons@gmail.com>
2025-12-28 18:39:39 -0700

    Auto: Edit sheet.tsx

--- src/components/ui/sheet/sheet.tsx
+++ src/components/ui/sheet/sheet.tsx
@@ -56,7 +56,7 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetPrimitive.Close className="absolute right-4 top-4 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-4 top-4 ring-offset-background hover:text-primary focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none cursor-pointer">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
